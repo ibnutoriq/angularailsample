df0bd8c93c26d903fc358132252bc6211c9fae08
Irfan Fadilah <irfan@41studio.com>
2014-10-26 15:07:49 +0700

    add angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+  
+  get 'directives', to: 'pages#directives', as: 'directives'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
